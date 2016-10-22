ec0937cc62f4e85ef6fb8d2a3d75c3a8e309afbd
sonlexus <ngocson092@gmail.com>
2016-10-22 11:53:58 +0700

    #2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import _ from 'lodash';
 import alertify from 'alertify.js';
 import helper from './helper';
+var initIdAddStart = 100;
+
 Vue.component('item', {
   template: '#folder-template',
   props: {
@@ -8,48 +11,66 @@ Vue.component('item', {
   },
   data: function () {
     return {
-
+      open:false
     }
   },
   computed: {
 
-    hasChildren:function () {
+    hasChildren() {
+
+      /*
+      * check item has children
+      * */
+
       return !!(this.model.children &&
           this.model.children.length)
     },
-    isActive:function(){
-      return this.model.id == this.model.id;
+    isActive(){
+
+        if(this.$root.currentFolderActive && this.$root.currentFolderActive.id){
+            return this.$root.currentFolderActive.id == this.model.id;
+        }
+        return false;
+
     }
     
   },
   ready(){
-    Vue.set(this,'open',false);
+
 
   },
   methods: {
     toggle: function () {
-      this.$parent.active = this.open;
       if (this.hasChildren) {
         this.open = !this.open
       }
-
+      Vue.set(this.$root, 'currentFolderActive',this.model);
     },
-    changeType: function () {
+    renameFolder: function () {
       var _this = this;
       alertify
-      .defaultValue("")
+      .defaultValue(_this.model.name)
       .prompt("Rename folder",
           function (val, ev) {
             ev.preventDefault();
-            if(val)
-              _this.model.name = val;
+
+            if(!val){
+              alertify.error("name folder not empty");
+              return;
+            }else if(val==_this.model.name){
+              //name not change
+              return;
+            }
+            _this.model.name = val;  
             alertify.success("rename folder successfully");
 
           }, function(ev) {
+            //cancel function goes here
+
             ev.preventDefault();
-          });
+          });//end alertify
     },
-    addChild: function () {
+    addFolder: function () {
 
       var _this = this;
       alertify
@@ -57,41 +78,78 @@ Vue.component('item', {
       .prompt("Enter new folder name",
           function (val, ev) {
             ev.preventDefault();
-
+            
+            if(!val){
+              alertify.error("name folder not empty");
+              return;
+            }
+            
             _this.model.children.push({
+              id: initIdAddStart,
               name: val,
-              parent_id:_this.model.id
+              parent_id:_this.model.id,
+              children: {}
             })
-
+            initIdAddStart++;
             alertify.success("add folder successfully");
 
           }, function(ev) {
             ev.preventDefault();
-      });
+          }
+      );//end alertify
 
 
 
-    }
-  }
+    }//end addFolder
+
+  }//end methods
+
+
 })
 
 new Vue({
   el: 'body',
   data: {
     folders: [
-      { id: 1, name: 'My Drive', parent_id: null ,children:null},
-      { id: 2, name: 'subfolder1', parent_id: 1  ,children:null},
-      { id: 3, name: 'subfolder2', parent_id: 1  ,children:null},
-      { id: 4, name: 'subfolder3', parent_id: 1  ,children:null},
-      { id: 5, name: 'subfolder2', parent_id: 4  ,children:null},
-      { id: 6, name: 'subfolder3', parent_id: 5  ,children:null}
+      { id: 1, name: 'My Drive', parent_id: null },
+      { id: 4, name: 'subfolder3', parent_id: 1  },
+      { id: 2, name: 'subfolder1', parent_id: 1  },
+      { id: 3, name: 'subfolder2', parent_id: 1  },
+      { id: 6, name: 'subfolder5', parent_id: 5  },
+      { id: 5, name: 'subfolder4', parent_id: 4  }
     ],
-    active:null
+    currentFolderActive:null
   },
   ready:function(){
-    Vue.set(this, 'folders', helper.convertHierarchical(this.folders))
+    this.convertData();
   },
   methods:{
+     addFolder(){
+         if(!this.currentFolderActive)
+             return;
+          this.currentFolderActive.children.push({
+             id: initIdAddStart,
+             name: 'new folder',
+             parent_id:this.currentFolderActive.id,
+             children:null
+         })
+         console.log(this.currentFolderActive.children);
+         initIdAddStart++;
+      },   
+     convertData(){
+       var _this = this;
 
+       _.forEach(_this.folders, function(value,i) {
+
+         Vue.set(_this.folders[i], 'children', null);
+
+         /*
+         * reactive properties "children"
+         *
+         * */
+
+       });
+       Vue.set(this, 'folders', helper.convertHierarchical(this.folders))
+     }
   }
 });
